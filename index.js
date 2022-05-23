2b6d21aa9a4ef688ae11ae822c90ce53be1642a5
Ety-Biswangri <etybgc02@gmail.com>
2022-05-23 16:01:04 +0600

    set up is done

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.biwlr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+client.connect(err => {
+    const partscollection = client.db("authentic-parts").collection("parts");
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello From Authentic Parts!');
+});
+
+app.listen(port, () => {
+    console.log(`Authentic Parts app listening on port ${port}`);
+});
